57b609ba648d5d1588e6210e599bb3d29e71f866
shige7ga <ameft_80@yahoo.co.jp>
2026-03-17 21:54:11 +0900

    add:ユーザ登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index]
+  resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
