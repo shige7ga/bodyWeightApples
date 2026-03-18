2a088fc1ec0e83f775229c427af53556b17dad5f
shige7ga <ameft_80@yahoo.co.jp>
2026-03-18 20:23:00 +0900

    add:ユーザーCRUD機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @user = User.new
@@ -15,10 +14,27 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to users_path, notice: 'ユーザ登録が完了しました'
     else
+      flash.now[:danger] = 'ユーザ登録に失敗しました'
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'ユーザ情報を編集しました'
+    else
+      flash.now[:danger] = '編集に失敗しました'
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, status: :see_other
   end
 
   private
