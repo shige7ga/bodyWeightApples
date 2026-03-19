20ba19e3794f858f0f3aadc21dc0d7b7701a3519
shige7ga <ameft_80@yahoo.co.jp>
2026-03-19 20:51:42 +0900

    add:bcrypt適用及びusersテーブルへパスワードカラム作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_18_120106) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_19_114506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_18_120106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "height", precision: 4, scale: 1
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
