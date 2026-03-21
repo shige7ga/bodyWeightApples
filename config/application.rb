3e34a7722c0598aaee322b0ff085b7b09844d91b
shige7ga <ameft_80@yahoo.co.jp>
2026-03-21 17:04:34 +0900

    edit:i18nによる日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
